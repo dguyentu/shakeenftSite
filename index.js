d5754d2b5baf04e04869db1f41a5b8b947e2e073
Dan Tu <73702049+dguyentu@users.noreply.github.com>
2022-05-21 10:47:24 -0400

    init

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-function showabout(){
-    $("#about_container").css("display","inherit");
+function showabout() {
+    $("#about_container").css("display", "inherit");
     $("#about_container").addClass("animated slideInLeft");
-    setTimeout(function(){
+    setTimeout(function () {
         $("#about_container").removeClass("animated slideInLeft");
-    },800);
+    }, 800);
 }
-function closeabout(){
+function closeabout() {
     $("#about_container").addClass("animated slideOutLeft");
-    setTimeout(function(){
+    setTimeout(function () {
         $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display","none");
-    },800);
+        $("#about_container").css("display", "none");
+    }, 800);
 }
-function showwork(){
-    $("#work_container").css("display","inherit");
-    $("#work_container").addClass("animated slideInRight");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideInRight");
-    },800);
+function showgallery() {
+    $("#gallery_container").css("display", "inherit");
+    $("#gallery_container").addClass("animated slideInRight");
+    setTimeout(function () {
+        $("#gallery_container").removeClass("animated slideInRight");
+    }, 800);
 }
-function closework(){
-    $("#work_container").addClass("animated slideOutRight");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
-        $("#work_container").css("display","none");
-    },800);
+function closegallery() {
+    $("#gallery_container").addClass("animated slideOutRight");
+    setTimeout(function () {
+        $("#gallery_container").removeClass("animated slideOutRight");
+        $("#gallery_container").css("display", "none");
+    }, 800);
 }
-function showcontact(){
-    $("#contact_container").css("display","inherit");
+function showcontact() {
+    $("#contact_container").css("display", "inherit");
     $("#contact_container").addClass("animated slideInUp");
-    setTimeout(function(){
+    setTimeout(function () {
         $("#contact_container").removeClass("animated slideInUp");
-    },800);
+    }, 800);
 }
-function closecontact(){
+function closecontact() {
     $("#contact_container").addClass("animated slideOutDown");
-    setTimeout(function(){
+    setTimeout(function () {
         $("#contact_container").removeClass("animated slideOutDown");
-        $("#contact_container").css("display","none");
-    },800);
+        $("#contact_container").css("display", "none");
+    }, 800);
 }
-setTimeout(function(){
+setTimeout(function () {
     $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-      $("#box").css("display","none");
-      $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
-      $("#work").removeClass("animated fadeIn");
-    },1000);
-},1500);
+    setTimeout(function () {
+        $("#loading").removeClass("animated fadeOut");
+        $("#loading").css("display", "none");
+        $("#box").css("display", "none");
+        $("#about").removeClass("animated fadeIn");
+        $("#contact").removeClass("animated fadeIn");
+        $("#gallery").removeClass("animated fadeIn");
+    }, 1000);
+}, 1500);
